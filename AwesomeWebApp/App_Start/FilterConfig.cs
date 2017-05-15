91363f537370ebf887bf8e2f8d46ebdf82688388
wely <wely_live@live.com>
2017-05-15 16:04:54 +0800

    add app insights

--- AwesomeWebApp/App_Start/FilterConfig.cs
+++ AwesomeWebApp/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace AwesomeWebApp
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
